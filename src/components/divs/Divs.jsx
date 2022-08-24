7e1db7bb4a13c4c4037bb9641beb144ff38441e7
Kestrel <a.cengiz2402@gmail.com>
2022-08-24 09:53:20 +0200

    ajout divs

---
+++ src/components/divs/Divs.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import tmdb from '../../api/tmdb';
+import Cards from '../cards/Cards'
+
+const Divs = () => {
+    const [moviePopular, setMoviePopular]= useState([]);
+
+    useEffect( () => {
+    const fetchMovies = async() => {
+        const {data} = await tmdb.get("movie/popular")
+        setMoviePopular(data.results)
+    }
+ 
+ fetchMovies()
+    }, [] )
+  return (
+    <div>
+        {moviePopular.map( (movie , index ) =>{
+            return <Cards key={index} {...movie}  /> 
+        })}
+    </div>
+  )
+}
+
+export default Divs
