55ef1a2253770d76039bb33b106d674b9abbc2ee
Kestrel <a.cengiz2402@gmail.com>
2022-08-24 15:46:05 +0200

    avancement react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Divs from './components/divs/Divs';
 
 function App() {
   return (
     <div className="App">
+      <Divs/>
     </div>
   );
 }
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import './Cards.css'
+
 
 const Poster= (posterpath) => {
     return `https://www.themoviedb.org/t/p/w440_and_h660_face${posterpath}`
 } 
 
 const Cards = ({overview, title, release_date, poster_path}) => {
+
+  
+let clr = (e) => {
+  if (e.target.style.color == 'red'){
+    e.target.style.color = 'black'
+    console.log(this.Cards);
+  } else {
+  e.target.style.color = 'red'
+  }
+}
 
   return (
     <div>
@@ -13,9 +25,8 @@ const Cards = ({overview, title, release_date, poster_path}) => {
             <p>{title}</p>
             <p>{overview}</p>
             <p>{release_date}</p>
-            <h1>Alixe est la meilleure des coachs</h1>
         </div>
-        <i>btn</i>
+        <i class="fa-solid fa-heart icon" onClick={clr}></i>
     </div>
   )
 }
